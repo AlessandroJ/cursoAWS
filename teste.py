84514cd3bf395529c44d35ad594b3dbec877c947
Alessandro <alessandrobernadino6@gmail.com>
2023-10-23 15:35:47 -0300

    segundo commit

--- teste.py
+++ teste.py
@@ -1,12 +1,18 @@
-nome = "Alessandro"
-
-escreva = (input("escreva por gentileza seu nome: "))
 
 
 
 
-if (escreva == nome):
-    print ("pode prosseguir")
+contador = 0
+while (contador < 5):
+           print(contador)
+           contador   = contador + 1
+
+contador = 0
+while (contador < 5):
+        print(contador)
+        contador = contador + 1
 else:
-    print ("Volte por favor")
+    print("O loop while foi encerrado com sucesso!")
 
+    
+    print(input("Agora sim, digite seu nome completo: "))
