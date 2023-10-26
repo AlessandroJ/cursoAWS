1474608c85314dd8856e010896ac87c88df40a2a
Alessandro <alessandrobernadino6@gmail.com>
2023-10-26 17:04:51 -0300

    Atividade de repetição

---
+++ repetição.py
@@ -0,0 +1,31 @@
+#Hoje é segunda-feira, e precisamos regar todas as plantas, imprime uma mensagem por cada planta pedindo ao robô para regá-la - Desafio
+# Extra, evidencie os numeros que forem par, identifique-os
+
+Contador = 0
+
+while(Contador <= 5):
+    if(Contador > 2):
+        print("regue as plantas", Contador)
+
+    Contador = Contador + 1
+
+
+
+
+    
+
+
+for Contador in range (3,6):
+    print("regue os vasos", Contador)
+        
+    
+
+
+
+
+
+
+for Contador in range(0,6,2):
+    if("os vasos que já contem batata já foram regados"):
+       print("Regue os vasos com tomates", Contador)
+
