2538c02427937b49a6b82f8178ebeabf7f55e6f7
Alessandro <alessandrobernadino6@gmail.com>
2023-11-07 16:18:54 -0300

    alteração na atividade

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,53 @@
-num1 = (input("Digite um numero: "))
-num2 = (input("Digite outro numero: "))
 
+def calculadora():
+    
+    while True:
+        print("Operações disponíveis:")
+        print("1: Soma")
+        print("2: Subtração")
+        print("3: Multiplicação")
+        print("4: Divisão")
+        print("0: Sair")
+    
+        escreva = input("Digite o comando da operação desejada:")
 
+        if(escreva == 0):
+            print("Sair da calculadora")
+            break
+        elif escreva in ('1','2','3','4'):
+            num1 = float(input("Qual irá ser o primeiro numero: "))
+            num2 = float(input("Qual irá ser seu segundo numero "))
 
-def calculadora(num1, num2, valor):
-    if(valor == "Soma"):
-        resultado = num1 + num2
+            if(escreva == '1'):
+                    resultado = num1 +num2        
+                    print(f"Resultado: {num1} + {num2} = {resultado}")
 
-    elif(valor == "subtração"):
-        resultado = num1 - num2
+            elif(escreva == '2'):
+                resultado = num1 - num2
+                print(f"Resultado: {num1} - {num2} = {resultado}")
+        
+            elif(escreva == '3'):
+                resultado = num1 * num2
+                print(f"Resultado: {num1} * {num2} = {resultado}")
+        
+            
 
-    elif(valor == "multiplicação"):
-        resultado = num1 * num2
-
-    elif(valor == "divisão"):
-        resultado = num1/num2
-        if num2 != 0:
-            resultado = num1/num2
-
+            elif(escreva == "4"):
+                if(num2 != 0):
+                    resultado = num1 / num2
+                    print(f"Resultado: {num1} / {num2} = {resultado}")
+            
+                else:
+                    print("Erro em sua divisão")
+            else:
+                print(". escolha uma operação valida" )
         else:
-            resultado = "Erro: Divisão por Zero"
+            print("Essa opção não existe. escolha uma opção valida fazendo favor")
 
-        return resultado
+    
+if __name__ == "__main__":
+    calculadora() 
+
 
 
     
